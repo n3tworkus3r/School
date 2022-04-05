bfcd824cccff9bbf2335691bd43c6210f41499c1
n3tworkus3r <network.us3r@gmail.com>
2022-04-05 21:17:52 +0300

    Updated directory locate

--- server.js
+++ server.js
@@ -3,47 +3,39 @@ const path = require('path')
 const fs = require('fs')
 
 const server = http.createServer((req,res) => {
-    if(req.method === 'GET') {
-        res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'})
-        
-        if(req.url === '/') {
-            fs.readFile(path.join(__dirname,'pages','index.html'), 'utf-8',
-            (err,content) => {
-                if(err) { throw err }
-                res.end(content)
-            })
-        } else if (req.method === 'POST') {
-            const body = []
+    
+    
+    switch(req.method) {
+        case 'GET':
+            res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'})
+            
+            // Выбор страницы для загрузки
+            switch(req.url) {
+                case '/': // Main page
+                    fs.readFile(path.join(__dirname,'src/pages','index.html'), 'utf-8',
+                    (err,content) => {
+                        if(err) { throw err }
+                        res.end(content)
+                    })
+                break
+                case '/about': // About page
+                    fs.readFile(path.join(__dirname,'src/pages','about.html'), 'utf-8',
+                    (err,content) => {
+                        if(err) { throw err }
+                        res.end(content)
+                    })
+                break
+            }
+        break
+        case 'POST':
+           
+        break
 
-            req.on('data', data => {
-                body.push(Buffer.from(data))
-            })
-        }
-        res.end(`
-        <h1>Ваше сообщение</h1>`)
-
-        /*
-        switch(req.url) {
-            case '/':
-                fs.readFile(path.join(__dirname,'pages','index.html'), 'utf-8',
-                (err,content) => {
-                    if(err) { throw err }
-                    res.end(content)
-                })
-            break
-            case '/about':
-                fs.readFile(path.join(__dirname,'pages','about.html'), 'utf-8',
-                (err,content) => {
-                    if(err) { throw err }
-                    res.end(content)
-                })
-            break
-  
-        } */
     }
 })
 
 
+// Запуск сервера
 server.listen(3000, () => {
     console.log('Server is running...')
 })
